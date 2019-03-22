6db238afeac64dc3a1940e7d05faeacf83bd5689
Laupretre Nans <nans.laupretre@gmail.com>
2019-03-22 19:53:48 +0100

    Finish The descent puzzle

---
+++ src/CondinGame.Puzzle.Easy/TheDescent/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CondinGame.Puzzle.Easy.TheDescent
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // game loop
+            while (true)
+            {
+                Dictionary<int, int> mountains = new Dictionary<int, int>(8);
+                for (int i = 0; i < 8; i++)
+                {
+                    int mountainH = int.Parse(Console.ReadLine());
+                    mountains.Add(i, mountainH);
+                }
+
+                KeyValuePair<int, int> mountainToShoot = mountains.OrderByDescending(m => m.Value).First();
+                Console.WriteLine(mountainToShoot.Key);
+            }
+        }
+    }
+}
