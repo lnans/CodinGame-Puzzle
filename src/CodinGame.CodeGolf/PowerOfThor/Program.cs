3daec9de7af4c75a10cd9213afa72bb8939cce21
Laupretre Nans <nans.laupretre@gmail.com>
2019-03-24 00:20:23 +0100

    Power of thor code golf update

--- src/CodinGame.CodeGolf/PowerOfThor/Program.cs
+++ src/CodinGame.CodeGolf/PowerOfThor/Program.cs
@@ -3,21 +3,17 @@ class Z
 {
 static void Main()
 {
-var i=R().Split(' ');
-int l=P(i[0]),m=P(i[1]),t=P(i[2]),u=P(i[3]);
+var i=C.ReadLine().Split(' ');
+int l=P(i[0]),m=P(i[1]),t=P(i[2]),u=P(i[3]),q=t-l,p=u-m;
 w:
-R();
-if(l>t&&m>u){M("SE");t++;u++;}
-if(l>t&&m==u){M("E");t++;}
-if(l>t&&m<u){M("NE");t++;u--;}
-if(l==t&&m>u){M("S");u++;}
-if(l==t&&m<u){M("N");u--;}
-if(l<t&&m==u){M("W");t--;}
-if(l<t&&m>u){M("SW");t--;u++;}
-if(l<t&&m<u){M("NW");t--;u--;}
+C.ReadLine();
+string y="",z="";
+if(p>0){y="N";p--;}
+if(p<0){y="S";p++;}
+if(q>0){z="W";q--;}
+if(q<0){z="E";q++;}
+C.WriteLine(y+z);
 goto w;
 }
-static void M(string d)=>C.WriteLine(d);
-static string R()=>C.ReadLine();
 static int P(string v)=>int.Parse(v);
 }
